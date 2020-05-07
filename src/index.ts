f4146168abbf6be6eac38be5b66d3833b0181b4f
Jawad Zaib <jawad@censeo-partners.com>
2020-05-07 20:12:00 +0500

    auth service exported

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import User from './models/User';
 import UserService from './services/UserService';
 import SocialAppService from './services/SocialAppService';
 import FacebookService from './services/social-accounts/FacebookService';
+import AuthService from './services/AuthService';
 
-export { SDKManager, UserService as Users, SocialAppService as SocialApp, FacebookService as Facebook, User };
+export {AuthService as Auth,  SDKManager, UserService as Users, SocialAppService as SocialApp, FacebookService as Facebook, User };
