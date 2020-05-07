77882f0b577a5615503c580208f31409d770c8f8
Jawad Zaib <jawad@censeo-partners.com>
2020-05-07 20:12:52 +0500

    patch version update

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,11 @@ import SocialAppService from './services/SocialAppService';
 import FacebookService from './services/social-accounts/FacebookService';
 import AuthService from './services/AuthService';
 
-export {AuthService as Auth,  SDKManager, UserService as Users, SocialAppService as SocialApp, FacebookService as Facebook, User };
+export {
+  AuthService as Auth,
+  SDKManager,
+  UserService as Users,
+  SocialAppService as SocialApp,
+  FacebookService as Facebook,
+  User,
+};
