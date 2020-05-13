e6a87aa4feb1a72a592c6dd7c2945b97fa1a2210
Jawad Zaib <jawad@censeo-partners.com>
2020-05-13 20:16:59 +0500

    port updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 // export const API_URL = 'https://backend-apis-v2.censeo-partners.com/api';
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'http://localhost:8000/api';
