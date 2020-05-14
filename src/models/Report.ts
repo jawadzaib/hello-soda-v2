ba980085a792ef8ffd593610bf5253f34c90294e
Jawad Zaib <jawad@censeo-partners.com>
2020-05-14 20:24:59 +0500

    patch version update

--- src/models/Report.ts
+++ src/models/Report.ts
@@ -5,18 +5,21 @@ class Report {
   private fraudScore: SocialScore;
   private overallScore: SocialScore;
   private employabilityScore: SocialScore;
-  private loanOutcomeScore : SocialScore;
+  private loanOutcomeScore: SocialScore;
   private employmentHistory: any[];
   constructor(data?: any) {
     this.scores = new Array<SocialScore>();
-    this.fraudScore = new SocialScore('Fraud Score', (data && data.scores.fraud) ? data.scores.fraud : '0')
-    this.scores.push(this.fraudScore)
-    this.overallScore = new SocialScore('Overall Score', (data && data.scores.overall) ? data.scores.overall : '0')
-    this.scores.push(this.overallScore)
-    this.employabilityScore = new SocialScore('Employability', ((data && data.scores.employed) ? data.scores.employed : '0') + '%')
-    this.scores.push(this.employabilityScore)
-    this.loanOutcomeScore = new SocialScore('Loan Outcome', '0')
-    this.scores.push(this.loanOutcomeScore)
+    this.fraudScore = new SocialScore('Fraud Score', data && data.scores.fraud ? data.scores.fraud : '0');
+    this.scores.push(this.fraudScore);
+    this.overallScore = new SocialScore('Overall Score', data && data.scores.overall ? data.scores.overall : '0');
+    this.scores.push(this.overallScore);
+    this.employabilityScore = new SocialScore(
+      'Employability',
+      (data && data.scores.employed ? data.scores.employed : '0') + '%',
+    );
+    this.scores.push(this.employabilityScore);
+    this.loanOutcomeScore = new SocialScore('Loan Outcome', '0');
+    this.scores.push(this.loanOutcomeScore);
     this.employmentHistory = new Array();
     if (data && data.employment) {
       this.employmentHistory = data.employment.history;
@@ -28,18 +31,17 @@ class Report {
   }
 
   getFraudScore() {
-      return this.fraudScore
+    return this.fraudScore;
   }
   getOverallScore() {
-      return this.overallScore
+    return this.overallScore;
   }
   getEmployabilityScore() {
-      return this.employabilityScore
+    return this.employabilityScore;
   }
   getLoanOutcomeScore() {
-      return this.loanOutcomeScore
+    return this.loanOutcomeScore;
   }
-
 }
 
 export default Report;
