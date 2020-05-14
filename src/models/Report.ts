9d758f0e120012a0372371467a2ae2262fb155f7
Jawad Zaib <jawad@censeo-partners.com>
2020-05-14 20:24:11 +0500

    social score updated

--- src/models/Report.ts
+++ src/models/Report.ts
@@ -2,15 +2,21 @@ import SocialScore from './SocialScore';
 
 class Report {
   private scores: SocialScore[];
+  private fraudScore: SocialScore;
+  private overallScore: SocialScore;
+  private employabilityScore: SocialScore;
+  private loanOutcomeScore : SocialScore;
   private employmentHistory: any[];
   constructor(data?: any) {
     this.scores = new Array<SocialScore>();
-    if (data && data.scores) {
-      this.scores.push(new SocialScore('Fraud Score', data.scores.fraud ? data.scores.fraud : '0'));
-      this.scores.push(new SocialScore('Overall Score', data.scores.overall) ? data.scores.overall : '0');
-      this.scores.push(new SocialScore('Employability', (data.scores.employed ? data.scores.employed : '0') + '%'));
-      this.scores.push(new SocialScore('Loan Outcome', '0'));
-    }
+    this.fraudScore = new SocialScore('Fraud Score', (data && data.scores.fraud) ? data.scores.fraud : '0')
+    this.scores.push(this.fraudScore)
+    this.overallScore = new SocialScore('Overall Score', (data && data.scores.overall) ? data.scores.overall : '0')
+    this.scores.push(this.overallScore)
+    this.employabilityScore = new SocialScore('Employability', ((data && data.scores.employed) ? data.scores.employed : '0') + '%')
+    this.scores.push(this.employabilityScore)
+    this.loanOutcomeScore = new SocialScore('Loan Outcome', '0')
+    this.scores.push(this.loanOutcomeScore)
     this.employmentHistory = new Array();
     if (data && data.employment) {
       this.employmentHistory = data.employment.history;
@@ -20,6 +26,20 @@ class Report {
   getScores() {
     return this.scores;
   }
+
+  getFraudScore() {
+      return this.fraudScore
+  }
+  getOverallScore() {
+      return this.overallScore
+  }
+  getEmployabilityScore() {
+      return this.employabilityScore
+  }
+  getLoanOutcomeScore() {
+      return this.loanOutcomeScore
+  }
+
 }
 
 export default Report;
